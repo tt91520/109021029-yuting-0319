fbcf4f8b4dc47a75e3ad42949b2d0e6bcf908afa
tt91520 <109021029@live.asia.edu.tw>
2021-03-26 15:58:26 +0800

    0326

--- demo.py
+++ demo.py
@@ -6,8 +6,18 @@ req.encoding = "utf8"
 if req.status_code == 200:
     #print(req.text)
     soup = BeautifulSoup(req.text,"lxml")
-    print(soup)
+    #print(soup)
     result1 = soup.find_all("li")
-    print(result1)
+    fp = open("out2.text", "w", encoding="utf8")
+    #print(result1)
+    #i = 1
+    for val in result1:
+        text2 = val.text.replace("\n", "")
+        text3 = text2.replace(" ","")
+        #print(i, val.text)
+        #i += 1
+        print(text3)
+        fp.write(text3 + "\n")
+    fp.close()
 else:
     print("沒有啦")
